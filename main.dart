123e8d5d4052b08170aa986518856ec3792ed5a9
Ziyauddin Shaik <112611879+Ziyauddin189@users.noreply.github.com>
2023-03-19 17:31:51 +0530

    Update main.dart

--- main.dart
+++ main.dart
@@ -1,15 +1,3 @@
-/*
-import 'package:flutter/material.dart';
-import 'package:project2/loginpage.dart';
-//import 'package:project2/registration_page.dart';
-void main(){
-  runApp(MaterialApp(
-    home:
-    const LoginPage()
-    )
-    );
-}*/
-
 import 'package:flutter/material.dart';
 import 'package:project2/home.dart';
 
